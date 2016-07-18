dde735000451df3a34fbe9aed20823ca5d4fdbe3
Vũ Quang Thịnh <=>
2016-07-18 15:32:04 +0700

    initital commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Yii2 Setting
+=========
+
+Usage
+-----
+
+// migrations
+yii migrate -p=@quangthinh/yii2-setting/migrations
+
+Tính năng setting cho trang
---
+++ migrations/m160718_042239_create_setting.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+class m160718_042239_create_setting extends Migration
+{
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+        $this->createTable('{{%setting}}', [
+            'key' => $this->string()->notNull(),
+            'section' => $this->string()->notNull(),
+            'data' => $this->text(),
+            'autoload' => $this->boolean()->defaultValue(true),
+        ]);
+
+        $this->addPrimaryKey('pk_setting', '{{%setting}}', ['key', 'section']);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%setting}}');
+    }
+
+}
